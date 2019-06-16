bdae013d4ba0027ec207b22e4bbb5818a28d20b1
marco.olea <marco.olea@ciencias.unam.mx>
2019-06-16 18:11:31 -0500

    Improved encryption process: The function now chooses a random integer x and forms groups of x bytes from the message's byte array. It then applies the RSA algorithm to the result of concatenating the numerical representation (0-255) of each byte in the group. The previous version simply applied the algorithm to each byte in the message's byte array.

--- crypto/rsa.py
+++ crypto/rsa.py
@@ -35,7 +35,7 @@ class PrivateKey:
 
 
 class RSA:
-    """The RSA cryptosystem for encrypting and decrypting strings.
+    """The RSA cryptosystem for encryption and decryption.
     
     Attributes:
         public_key: The public parameters.
@@ -50,6 +50,12 @@ class RSA:
     
     SECURITY = 10
     """Security parameter for the Miller-Rabin probabilistic primality test."""
+    
+    MAX_GROUP_SIZE = 30
+    """Maximum number of bytes to group together."""
+    
+    DELIMITER = '#'
+    """Delimiter character that should not be used in any Base64 encoding implementation."""
     
     def __init__(self, public_key: PublicKey, private_key: PrivateKey):
         """Initialize the cryptosystem with the given keys."""
@@ -74,7 +80,7 @@ class RSA:
         
         # Choose a random e such that 1 < e < φ(n) and gcd(e, φ(n)) = 1.
         # If we choose a random prime instead of any random number, we only need to check if φ(n)
-        # is a multiple of e to verify that phi_n and e are relatively prime.
+        # is a multiple of e to verify that φ(n) and e are relatively prime.
         e = 1
         while phi_n % e == 0:
             # Clearly φ(n) is at least 200 decimal digits. The database does not store primes of
@@ -90,43 +96,42 @@ class RSA:
     def encrypt(self, message: str) -> str:
         """Encrypt a string.
         
-        Args:
-            message: The string to encrypt.
-        Returns:
-            The result of applying the RSA algorithm for encryption.
+        Note that all occurrences of the NULL character U+0000 will be lost in the encryption
+        process.
         """
-        # Apply m^e % n = c to each byte m in the message, and convert c to string.
-        transformed_numbers = [
-            str(power(m, self.public_key.e, self.public_key.n))
-            for m in message.encode()]  # message.encode() -> utf-8 byte array
+        # Get the UTF-8 byte array of the message, and convert each byte into a string of 3 digits.
+        message = [f'{b:03}' for b in message.encode()]
+        length = len(message)
+        # Choose the size of each group of bytes.
+        group_size = randint(1, length if length < self.MAX_GROUP_SIZE else self.MAX_GROUP_SIZE)
+        # Append ['000'] until len(message) is a multiple of group_size.
+        message += ['000'] * (group_size - length % group_size)
+        # Join into groups.
+        groups = [message[i: i + group_size] for i in range(0, length, group_size)]
+        # Numbers to which the transformation will be applied.
+        numbers = [int(''.join(group)) for group in groups]
+        # Apply m^e % n = c to each number m, and convert c to a string.
+        transformed_numbers = [str(power(m, self.public_key.e, self.public_key.n)) for m in numbers]
         # Pre-appends '' or '0' to make sure each string has an even number of digits.
         transformed_numbers = [
             ('' if len(transformed_number) % 2 == 0 else '0') + transformed_number
             for transformed_number in transformed_numbers]
-        # Now we split each every transformed number into groups of two digits that represent bytes.
+        # Now we split each every transformed number into pairs of digits that represent bytes.
         # Three-digit numbers might not be in [0, 255], but two-digit numbers always will be.
         byte_arrays = [
             [int(c[i: i + 2]) for i in range(0, len(c), 2)]
             for c in transformed_numbers]
-        # Encode the byte arrays with base64.
+        # Encode the resulting byte arrays with base64.
         b64_strings = [
             b64encode(bytes(byte_array)).decode()  # message.decode() -> utf-8 string
             for byte_array in byte_arrays]
-        # Return the strings separated by newlines.
-        return '\n'.join(b64_strings)
+        # Return the strings separated by a delimiter.
+        return self.DELIMITER.join(b64_strings)
     
     def decrypt(self, message: str) -> str:
-        """Decrypt a string.
-
-        Args:
-            message: The string to decrypt.
-        Returns:
-            The result of applying the RSA algorithm for decryption.
-        """
+        """Decrypt a string that was encrypted with this class's encrypt method."""
         # Decode each string with base64.
-        b64_arrays = [
-            b64decode(line.encode())
-            for line in message.strip().split()]
+        b64_arrays = [b64decode(line.encode()) for line in message.split(self.DELIMITER)]
         # Get the int (two-digit) representation of each byte in every byte array.
         byte_arrays = [
             [int(b) for b in b64_array]
@@ -136,9 +141,14 @@ class RSA:
         transformed_numbers = [
             int(''.join([f'{b:02}' for b in byte_array]))
             for byte_array in byte_arrays]
-        # Apply c^d % n = m to each transformed number c to get the original byte m.
-        original_bytes = [
-            power(c, self.private_key.d, self.private_key.n)
+        # Apply c^d % n = m to each transformed number c to get the original number m.
+        numbers = [
+            str(power(c, self.private_key.d, self.private_key.n))
             for c in transformed_numbers]
-        # Return the single resulting byte array as a string.
-        return bytes(original_bytes).decode()
+        # Split each number m (from tight to left) into three-digit numbers.
+        decrypted_bytes = [
+            int(m[(0 if i < 3 else i - 3): i])
+            for m in numbers[::-1] for i in range(len(m), 0, -3)]
+        # Reverse and remove all zeros.
+        original_byte_array = [dec_byte for dec_byte in decrypted_bytes[::-1] if dec_byte != 0]
+        return bytes(original_byte_array).decode()
